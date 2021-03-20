0a44ecf3b7a587a9444cdbdb3651cb5251296be1
kostyatimoshuk <kostyatimka@gmail.com>
2021-03-20 12:36:21 +0200

    change app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import Authors from "./components/authors";
+import Comments from "./components/comments";
+
+import { Comment } from "./types";
+
+import "./App.css";
+
+const comments = [
+	{
+    	id: 1,
+		author: "Nikolay",
+		message: "Hi",
+		comments: [
+			{
+        		id: 3,
+				author: "Olga",
+				message: "Hi there",
+				comments: [],
+			},
+			{
+        		id: 4,
+				author: "Nikolay",
+				message: "What's up?!",
+				comments: [
+					{
+            			id: 5,
+						author: "Olga",
+						message: "How are u?",
+						comments: [],
+					},
+				],
+			},
+		],
+	},
+	{
+    	id: 2,
+		author: "Alex",
+		message: "Hey folks!",
+		comments: [],
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	const [selectedAuthor, setSelectedAuthor] = useState<string>("");
+	const [authors, setAuthors] = useState<string[]>([]);
+	const [uniqueAuthors, setUniqueAuthors] = useState<string[]>([]);
+
+	const clearSelectedAuthor = () => {
+		setSelectedAuthor("");
+	};
+	
+	const getAuthors = useCallback((comments: Comment[]) => {
+		comments.forEach((comment: Comment) => {
+			setAuthors((prevState) => [...prevState, comment.author])
+
+			if (comment.comments.length > 0) {
+				getAuthors(comment.comments)
+			}
+		})
+
+		return authors
+	}, []);
+
+	useEffect(() => {
+		getAuthors(comments)
+	}, [getAuthors]);
+
+	useEffect(() => {
+		const unique = authors.filter((author, idx, arr) => arr.indexOf(author) === idx)
+
+		setUniqueAuthors(unique)
+	}, [authors]);
+
+	return (
+		<div className="App">
+			<h1>Комментарии</h1>
+			<button onClick={clearSelectedAuthor}>Clear selected</button>
+			<Comments comments={comments} selectedAuthor={selectedAuthor} onUserSelect={setSelectedAuthor} />
+			<Authors authors={uniqueAuthors} />
+		</div>
+	);
 }
 
 export default App;
